9cb86a084dc5e648c248148961a9c738dfa6bc53
e5022444 <e5022444@TCLHSRNVALT0084>
2025-09-05 09:46:01 +0530

    Add project files.

---
+++ EnumGenerator/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace EnumGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtOutputPath = new System.Windows.Forms.TextBox();
+            this.btnBrowseOutput = new System.Windows.Forms.Button();
+            this.txtCsvPath = new System.Windows.Forms.TextBox();
+            this.btnBrowseCsv = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNamespace = new System.Windows.Forms.TextBox();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enum Generator";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(296, 402);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(117, 36);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.txtOutputPath);
+            this.panel1.Controls.Add(this.btnBrowseOutput);
+            this.panel1.Controls.Add(this.txtCsvPath);
+            this.panel1.Controls.Add(this.btnBrowseCsv);
+            this.panel1.Location = new System.Drawing.Point(12, 55);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(764, 132);
+            this.panel1.TabIndex = 6;
+            // 
+            // txtOutputPath
+            // 
+            this.txtOutputPath.Location = new System.Drawing.Point(181, 86);
+            this.txtOutputPath.Name = "txtOutputPath";
+            this.txtOutputPath.Size = new System.Drawing.Size(553, 22);
+            this.txtOutputPath.TabIndex = 8;
+            // 
+            // btnBrowseOutput
+            // 
+            this.btnBrowseOutput.Location = new System.Drawing.Point(31, 79);
+            this.btnBrowseOutput.Name = "btnBrowseOutput";
+            this.btnBrowseOutput.Size = new System.Drawing.Size(117, 36);
+            this.btnBrowseOutput.TabIndex = 7;
+            this.btnBrowseOutput.Text = "Output File";
+            this.btnBrowseOutput.UseVisualStyleBackColor = true;
+            this.btnBrowseOutput.Click += new System.EventHandler(this.btnBrowseOutput_Click);
+            // 
+            // txtCsvPath
+            // 
+            this.txtCsvPath.Location = new System.Drawing.Point(181, 25);
+            this.txtCsvPath.Name = "txtCsvPath";
+            this.txtCsvPath.Size = new System.Drawing.Size(553, 22);
+            this.txtCsvPath.TabIndex = 6;
+            // 
+            // btnBrowseCsv
+            // 
+            this.btnBrowseCsv.Location = new System.Drawing.Point(31, 18);
+            this.btnBrowseCsv.Name = "btnBrowseCsv";
+            this.btnBrowseCsv.Size = new System.Drawing.Size(117, 36);
+            this.btnBrowseCsv.TabIndex = 5;
+            this.btnBrowseCsv.Text = "Select CSV";
+            this.btnBrowseCsv.UseVisualStyleBackColor = true;
+            this.btnBrowseCsv.Click += new System.EventHandler(this.btnBrowseCsv_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel2.Controls.Add(this.txtClass);
+            this.panel2.Controls.Add(this.txtNamespace);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(12, 204);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(764, 169);
+            this.panel2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 30);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "namespace : ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 30);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "class : ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtNamespace
+            // 
+            this.txtNamespace.Location = new System.Drawing.Point(181, 30);
+            this.txtNamespace.Name = "txtNamespace";
+            this.txtNamespace.Size = new System.Drawing.Size(377, 22);
+            this.txtNamespace.TabIndex = 7;
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(181, 85);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(377, 22);
+            this.txtClass.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtOutputPath;
+        private System.Windows.Forms.Button btnBrowseOutput;
+        private System.Windows.Forms.TextBox txtCsvPath;
+        private System.Windows.Forms.Button btnBrowseCsv;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.TextBox txtNamespace;
+    }
+}
+
---
+++ EnumGenerator/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnumGenerator
+{
+    public partial class Form1 : Form
+    {
+
+        string defaultnamespaceName = "DIO";
+        string defaultclassName = "DIDO";
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtNamespace.Text = defaultnamespaceName;
+            txtClass.Text = defaultclassName;
+        }
+
+        private void btnBrowseCsv_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtCsvPath.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void btnBrowseOutput_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "C# Files (*.cs)|*.cs";
+                saveFileDialog.FileName = "DIDOEnums.cs";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtOutputPath.Text = saveFileDialog.FileName;
+                }
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string csvPath = txtCsvPath.Text;
+                string outputPath = txtOutputPath.Text;
+
+                string namespaceName = string.IsNullOrWhiteSpace(txtNamespace.Text) ? defaultnamespaceName : txtNamespace.Text.Trim();
+                string className = string.IsNullOrWhiteSpace(txtClass.Text) ? defaultclassName : txtClass.Text.Trim();
+
+                if (!File.Exists(csvPath))
+                {
+                    MessageBox.Show("Please select a valid CSV file.");
+                    return;
+                }
+
+                var lines = File.ReadAllLines(csvPath)
+                                .Skip(1) // skip header
+                                .Select(l => l.Split(','))
+                                .ToList();
+
+                var inputs = lines.Where(l => l[1].Equals("Input", StringComparison.OrdinalIgnoreCase))
+                                  .Select(l => new { Name = l[5].Trim(), Id = l[4].Trim(), IoName = l[6].Trim() })
+                                  .ToList();
+
+                var outputs = lines.Where(l => l[1].Equals("Output", StringComparison.OrdinalIgnoreCase))
+                                   .Select(l => new { Name = l[5].Trim(), Id = l[4].Trim(), IoName = l[6].Trim() })
+                                   .ToList();
+
+                using (StreamWriter writer = new StreamWriter(outputPath))
+                {
+                    writer.WriteLine($"namespace {namespaceName}");
+                    writer.WriteLine("{");
+                    writer.WriteLine($"\tpublic partial class {className}");
+                    writer.WriteLine("\t{");
+
+
+                    // EInput
+                    if (inputs.Any())
+                    {
+                        writer.WriteLine("\t\tpublic enum EInput");
+                        writer.WriteLine("\t\t{");
+                        writer.WriteLine("\t\t\tStart = -1,");
+
+                        foreach (var inp in inputs)
+                            writer.WriteLine($"\t\t\t{inp.Name} = {inp.Id}, // {inp.IoName}");
+
+                        writer.WriteLine("\t\t\tEnd");
+                        writer.WriteLine("\t\t}");
+                        writer.WriteLine();
+                    }
+
+                    // EOutput
+                    if (outputs.Any())
+                    {
+                        writer.WriteLine("\t\tpublic enum EOutput");
+                        writer.WriteLine("\t\t{");
+                        writer.WriteLine("\t\t\tStart = -1,");
+
+                        foreach (var outp in outputs)
+                            writer.WriteLine($"\t\t\t{outp.Name} = {outp.Id}, // {outp.IoName}");
+
+                        writer.WriteLine("\t\t\tEnd");
+                        writer.WriteLine("\t\t}");
+                        writer.WriteLine();
+                    }
+
+                    writer.WriteLine("\t}");
+                    writer.WriteLine("}");
+                }
+
+
+                MessageBox.Show("C# enum file generated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
